7da12293d6bedf7d6a8b680b95129c7f92d3f4b0
AndyE <andye456@gmail.com>
2015-09-25 21:06:25 +0100

    Added all files needed to get up and running

---
+++ install.sh
@@ -0,0 +1,6 @@
+# configures the app.
+# puts the required libs in the pubic directory so can be used by Angular
+
+cp node_modules/c3/c3.js public/
+cp node_modules/c3/c3.css public/
+cp node_modules/d3/d3.js public/
