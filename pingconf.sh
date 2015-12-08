350f1ae6e6ba9c1b1d580eacea1127b485b8d641
AndyE <andye456@gmail.com>
2015-12-08 23:16:29 +0000

    made boot menu better

--- pingconf.sh
+++ pingconf.sh
@@ -1,68 +1,117 @@
 #!/bin/bash
 # Main Menu - used to set the main parameters
-# 1. default gateway, i.e. the IP addres of your router
-# 2. mask - usually 255.255.255.0
-# 3. SSID
-# 4. WPA Password
-# 5. LAN address (e.g. 192.168.1.17)
-# 6. LAN port, e.g. 8080
-# 7. External website IP
+# 1. system - default gateway, i.e. the IP addres of your router
+# 2. system - mask - usually 255.255.255.0
+# 3. system - SSID
+# 4. system - WPA Password
+# 5. system - LAN address (e.g. 192.168.1.17)
+# 6. node.js - LAN port, e.g. 8080
+# 7. node.js - External website IP
 
 
+# Global variables
+                                                                                                       
+setInterfaces() {
 
-configfile=~/.pingpirc
+# Ooo this looks rather complicated, well I can never remember stuff like this it just makes sure you can run the command as root.
+# If you don't put the /dev/null bit in then it does the tee before the sudo and fails.
+# Cool or what!
+sudo tee /etc/network/interfaces.tmp > /dev/null << _EOF
+auto lo
+iface lo inet loopback
+iface eth0 inet dhcp
+allow-hotplug wlan0
+auto wlan0
+iface wlan0 inet static
+wpa-ssid $4
+wpa-psk $5
+address $3
+netmask $2
+gateway $1
 
-echo "..Reading Properties file.."
-if [ -r $configfile ]
+_EOF
+
+} # END setInterfaces
+
+get_user_input() {
+	configfile=~/.pingpirc
+
+	echo "..Reading Properties file.."
+	if [ -r $configfile ]
+	then
+		source "$configfile"
+	else
+		echo ".pingpirc not found - will be created following this config"
+	fi
+
+	echo "------------------------------------------------"
+	echo "Please enter the properties for initial setup."
+	echo "These can be changed from the GUI once started"
+	echo "------------------------------------------------"
+	echo
+	echo -n "Default gateway [$default_gateway] "
+	read input
+	[[ $input != "" ]] && default_gateway=$input
+
+	echo -n "Mask            [$mask] "
+	read input 
+	[[ $input != "" ]] && mask=$input
+
+	echo -n "SSID            [$ssid] "
+	read input
+	[[ $input != "" ]] && ssid=$input
+
+	echo -n "WPA Password    [$password] "
+	read input
+	[[ $input != "" ]] && password=$input
+
+	echo -n "LAN address     [$lanip] "
+	read input
+	[[ $input != "" ]] && lanip=$input
+
+	echo -n "LAN port        [$lanport] "
+	read input
+	[[ $input != "" ]] && lanport=$input
+
+	echo -n "External IP     [$extip] "
+	read input
+	[[ $input != "" ]] && extip=$input
+	
+	check_ok
+}
+
+check_ok() {
+	echo -n "Correct y/n or q ? "
+	read input
+	if [[ $input == "y" ]]
+	then
+		# Write the properties to the props file
+		echo "default_gateway=$default_gateway" > $configfile
+		echo "mask=$mask" >> $configfile
+		echo "ssid=$ssid" >> $configfile
+		echo "password=$password" >> $configfile
+		echo "lanip=$lanip" >> $configfile
+		echo "lanport=$lanport" >> $configfile
+		echo "extip=$extip" >> $configfile
+		setInterfaces $default_gateway $mask $lanip $ssid $password
+	elif [[ $input == "n" ]]
+	then
+		get_user_input
+	elif [[ $input == "q" ]]
+	then
+		:
+	else
+		check_ok
+	fi
+
+}
+
+# If parameters are given then use them to set the config
+# This is so it can be called from server.js when set from web page.
+if [[ $# -eq 5 ]]
 then
-	source "$configfile"
+	setInterfaces $1 $2 $3 $4 $5
 else
-	echo ".pingpirc not found - will be created following this config"
+	get_user_input
 fi
 
-echo "------------------------------------------------"
-echo "Please enter the properties for initial setup."
-echo "These can be changed from the GUI once started"
-echo "------------------------------------------------"
-echo
-echo -n "Default gateway [$default_gateway] "
-read input
-[[ $input != "" ]] && default_gateway=$input
-
-echo -n "Mask            [$mask] "
-read input 
-[[ $input != "" ]] && mask=$input
-
-echo -n "SSID            [$ssid] "
-read input
-[[ $input != "" ]] && ssid=$input
-
-echo -n "WPA Password    [$password] "
-read input
-[[ $input != "" ]] && password=$input
-
-echo -n "LAN address     [$lanip] "
-read input
-[[ $input != "" ]] && lanip=$input
-
-echo -n "LAN port        [$lanport] "
-read input
-[[ $input != "" ]] && lanport=$input
-
-echo -n "External IP     [$extip] "
-read input
-[[ $input != "" ]] && extip=$input
-
-# Write the properties to the props file
-echo "default_gateway=$default_gateway" > $configfile
-echo "mask=$mask" >> $configfile
-echo "ssid=$ssid" >> $configfile
-echo "password=$password" >> $configfile
-echo "lanip=$lanip" >> $configfile
-echo "lanport=$lanport" >> $configfile
-echo "extip=$extip" >> $configfile
-
-# Write the values to the various config locations
-# restart demons if necessary, 
-/etc/network/interfaces
-e.g. ifdown eth01, ifup eth01 or whatever
